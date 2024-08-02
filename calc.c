a50cac53fde7106a04c47c5c6e4d573b31844679
Cal_M1 <175713214+Cal-M1@users.noreply.github.com>
2024-08-02 13:20:23 +0100

    ❕ Bug fixes + Added Loop to Repeat Program

--- calc.c
+++ calc.c
@@ -27,6 +27,7 @@ double simplCalc(double num1, double num2, char op)
     default:
     printf("Error! Please enter a vaild Operator\n");
     }
+    
     return result;
 }
 
@@ -34,27 +35,37 @@ int main()
 {
     char op;
     double num1, num2, result;
+    char choice;
 
-    // Asks the user to input a Operator
-    printf("Enter an operator (+, -, *, /): ");
-    // Reads Operator
-    scanf(" %c", &op);
+    do{
+        // Asks the user to input a Operator
+        printf("Enter an operator (+, -, *, /): ");
+        // Reads Operator
+        scanf(" %c", &op);
 
-    // Asks the user to input Number 1
-    printf("Enter Number 1: ");
-    // Reads num1
-    scanf("%lf", &num1);
+        // Asks the user to input Number 1
+        printf("Enter Number 1: ");
+        // Reads num1
+        scanf("%lf", &num1);
 
-    // Asks the user to input Number 1
-    printf("Enter Number 2: ");
-    // Reads num1
-    scanf("%lf", &num2);
+        // Asks the user to input Number 2
+        printf("Enter Number 2: ");
+        // Reads num2
+        scanf("%lf", &num2);
 
-    // Performs the calculation
-    result = simplCalc(num1, num2, op);
+        // Performs the calculation
+        result = simplCalc(num1, num2, op);
 
-    // Outputs the result
-    printf("Result = %.2lf\n", result);
+        // Outputs the result
+        printf("Result = %.2lf\n", result);
 
+        // ask the user if they want to calculate again
+        printf("Printf would you like to calculate another set of numbers? (y/n): ");
+        // reads user choice
+        scanf(" %c", &choice);
+
+    // Loop that repeat the program if user choice = y
+    }while(choice == 'y' || choice =='Y');
+    
     return 0;
 }
